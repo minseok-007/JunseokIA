9f922f4f7291cda2d22ba8cbd9434012a42472b6
Minseok Chey <mc1920@ic.ac.uk>
2024-07-21 19:42:38 +0900

    Initial Commit

---
+++ Secondhand_Market/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Secondhand_Market
+//
+//  Created by Minseok Chey on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Secondhand_Market/Secondhand_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secondhand_MarketApp.swift
+//  Secondhand_Market
+//
+//  Created by Minseok Chey on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Secondhand_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
